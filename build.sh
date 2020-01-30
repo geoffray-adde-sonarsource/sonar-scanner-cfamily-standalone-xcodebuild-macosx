ef15ab0c707e8c3f964a7cefb76376d8008a931e
Geoffray Adde <geoffray.adde@sonarsource.com>
2020-01-30 09:05:46 +0100

    cleanup

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 # use only one of SonarQube or SonarCloud
 
 # Configuration for SonarQube
-SONAR_URL="http://localhost:9000" # URL of the SonarQube server
+SONAR_HOST_URL="http://localhost:9000" # URL of the SonarQube server
 SONAR_TOKEN=f5f56032a938d29cf76d78de33991eb8c273a0ea # access token from SonarQube projet creation page -Dsonar.login=XXXX
 SONAR_PROJECT_KEY=sonar_scanner_example # project name from SonarQube projet creation page -Dsonar.projectKey=XXXX
 SONAR_PROJECT_NAME=sonar_scanner_example # project name from SonarName projet creation page -Dsonar.projectName=XXXX
@@ -13,12 +13,12 @@ SONAR_PROJECT_NAME=sonar_scanner_example # project name from SonarName projet cr
 #SONAR_PROJECT_KEY= # project name from SonarCloud projet creation page -Dsonar.projectKey=XXXX
 #SONAR_ORGANIZATION= # organization name from SonarCloud projet creation page -Dsonar.organization=ZZZZ
 
-# Set default to SONAR_URL in not provided
-SONAR_URL=${SONAR_URL:-https://sonarcloud.io}
+# Set default to SONAR_HOST_URL in not provided
+SONAR_HOST_URL=${SONAR_HOST_URL:-https://sonarcloud.io}
 
 # Download build-wrapper
 rm -rf build-wrapper-macosx-x86.zip build-wrapper-macosx-x86
-curl "${SONAR_URL}/static/cpp/build-wrapper-macosx-x86.zip" --output build-wrapper-macosx-x86.zip
+curl "${SONAR_HOST_URL}/static/cpp/build-wrapper-macosx-x86.zip" --output build-wrapper-macosx-x86.zip
 unzip build-wrapper-macosx-x86.zip
 
 # Download sonar-scanner
@@ -38,4 +38,4 @@ build-wrapper-macosx-x86/build-wrapper-macosx-x86 --out-dir build_wrapper_output
 [ -n "$SONAR_ORGANIZATION" ] && SONAR_ORGANIZATION_CMD_ARG="-Dsonar.organization=${SONAR_ORGANIZATION}"
 [ -n "$SONAR_PROJECT_NAME" ] && SONAR_PROJECT_NAME_CMD_ARG="-Dsonar.projectName=${SONAR_PROJECT_NAME}"
 SONAR_OTHER_ARGS="-Dsonar.projectVersion=1.0 -Dsonar.sources=sonar_scanner_example -Dsonar.cfamily.build-wrapper-output=build_wrapper_output_directory -Dsonar.sourceEncoding=UTF-8"
-sonar-scanner/bin/sonar-scanner -Dsonar.host.url="${SONAR_URL}" -Dsonar.projectKey=${SONAR_PROJECT_KEY} ${SONAR_OTHER_ARGS} ${SONAR_PROJECT_NAME_CMD_ARG} ${SONAR_TOKEN_CMD_ARG} ${SONAR_ORGANIZATION_CMD_ARG}
+sonar-scanner/bin/sonar-scanner -Dsonar.host.url="${SONAR_HOST_URL}" -Dsonar.projectKey=${SONAR_PROJECT_KEY} ${SONAR_OTHER_ARGS} ${SONAR_PROJECT_NAME_CMD_ARG} ${SONAR_TOKEN_CMD_ARG} ${SONAR_ORGANIZATION_CMD_ARG}
